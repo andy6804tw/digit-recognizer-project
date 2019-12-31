dccadadefa95a86d3fe6aaf2af15cf6e2824d787
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2019-12-31 12:30:24 +0800

    add click animate

--- web/js/animate.js
+++ web/js/animate.js
@@ -19,3 +19,18 @@ marker.addEventListener('animationend', function(){
 centerComposition.addEventListener('animationend', function(){
   centerComposition.classList.add('start');
 });
+
+const start=()=>{
+  const sectionAnimate=document.getElementById("sectionAnimate");
+  sectionAnimate.classList.remove('d-none');
+  
+  const itemClock=document.getElementById("itemClock");
+  const pinkBall=document.getElementById("pinkBall");
+  const pinkArrow=document.getElementById("pinkArrow");
+  const dashPink=document.getElementById("dashedPink");
+
+  itemClock.classList.add("scale-animation","delay-730");
+  pinkBall.classList.add("scale-animation","delay-830");
+  pinkArrow.classList.add("scale-animation","delay-1030");
+  dashPink.classList.add("dashed-pink");
+}
