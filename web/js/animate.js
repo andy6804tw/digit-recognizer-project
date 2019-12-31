42326b04b0ec37c679a85f95219843e3417a5b70
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2019-12-31 14:42:29 +0800

    js check scrollTop get hight

--- web/js/animate.js
+++ web/js/animate.js
@@ -4,33 +4,41 @@ var sun = document.querySelector('.sun');
 var marker = document.querySelector('.marker');
 var centerComposition = document.querySelector('.center-composition');
 
-minutes.addEventListener('animationend', function(){
+minutes.addEventListener('animationend', function () {
   minutes.classList.add('start');
 });
 
-sun.addEventListener('animationend', function(){
+sun.addEventListener('animationend', function () {
   sun.classList.add('start');
 });
 
-marker.addEventListener('animationend', function(){
+marker.addEventListener('animationend', function () {
   marker.classList.add('start');
 });
 
-centerComposition.addEventListener('animationend', function(){
+centerComposition.addEventListener('animationend', function () {
   centerComposition.classList.add('start');
 });
 
-const start=()=>{
-  const sectionAnimate=document.getElementById("sectionAnimate");
+const start = () => {
+  const sectionAnimate = document.getElementById("sectionAnimate");
   sectionAnimate.classList.remove('d-none');
-  
-  const itemClock=document.getElementById("itemClock");
-  const pinkBall=document.getElementById("pinkBall");
-  const pinkArrow=document.getElementById("pinkArrow");
-  const dashPink=document.getElementById("dashedPink");
 
-  itemClock.classList.add("scale-animation","delay-730");
-  pinkBall.classList.add("scale-animation","delay-830");
-  pinkArrow.classList.add("scale-animation","delay-1030");
+  const itemClock = document.getElementById("itemClock");
+  const pinkBall = document.getElementById("pinkBall");
+  const pinkArrow = document.getElementById("pinkArrow");
+  const dashPink = document.getElementById("dashedPink");
+
+  itemClock.classList.add("scale-animation", "delay-730");
+  pinkBall.classList.add("scale-animation", "delay-830");
+  pinkArrow.classList.add("scale-animation", "delay-1030");
   dashPink.classList.add("dashed-pink");
 }
+
+document.body.onscroll = function (event) {
+  const html = document.documentElement;
+  if (html.scrollTop > 1000) {
+    const sectionAnimate = document.getElementById("sectionAnimate");
+    sectionAnimate.classList.remove('d-none');
+  }
+};
