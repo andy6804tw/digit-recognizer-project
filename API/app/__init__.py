b2fd16f938360af324e644bcd04e1fd9532a3871
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2020-01-01 10:19:15 +0800

    remove router

--- API/app/__init__.py
+++ API/app/__init__.py
@@ -2,13 +2,10 @@ import config
 from flask import Flask,request,render_template
 from flask_cors import CORS
 from app.controllers.mnist import mnistCtrl 
-from flask_uploads import configure_uploads
-from app.modules.upload import upFile
 
 
 app=Flask(__name__)
 CORS(app)
 app.config.from_object(config) # 由config.py管理環境變數
-configure_uploads(app, upFile)
 
 app.register_blueprint(mnistCtrl, url_prefix='/mnist')
--- API/config.py
+++ API/config.py
@@ -1,5 +1,3 @@
 DEBUG = True
 PORT=5001
 ENV='development'
-SECRET_KEY = 'files'
-UPLOADED_UPFILE_DEST = 'app/static'
