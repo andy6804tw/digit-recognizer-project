939fc522adabe1abc77993c8931483ee724bfe44
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2020-01-01 10:18:36 +0800

    remove router

--- API/app/__init__.py
+++ API/app/__init__.py
@@ -2,7 +2,6 @@ import config
 from flask import Flask,request,render_template
 from flask_cors import CORS
 from app.controllers.mnist import mnistCtrl 
-from app.controllers.upload import uploadCtrl
 from flask_uploads import configure_uploads
 from app.modules.upload import upFile
 
@@ -13,4 +12,3 @@ app.config.from_object(config) # 由config.py管理環境變數
 configure_uploads(app, upFile)
 
 app.register_blueprint(mnistCtrl, url_prefix='/mnist')
-app.register_blueprint(uploadCtrl, url_prefix='/upload')
