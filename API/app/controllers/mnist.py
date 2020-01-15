6f117cd8adbb3ab9770a5298b84b5120ac12c518
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2020-01-15 10:50:27 +0800

    mnist ctrl add POST method

--- API/app/controllers/mnist.py
+++ API/app/controllers/mnist.py
@@ -1,15 +1,23 @@
 from flask import Blueprint, request,jsonify,redirect
 import app.modules.mnist as mnistModule
+import base64
 
 mnistCtrl = Blueprint('mnist',__name__)
   
 
 @mnistCtrl.route('', methods=['GET','POST'])
 def getResult():
+  base64Image=''
   if request.method=='GET':
     return jsonify({'result':str(mnistModule.getResult())})
   else:
-    insertValues = request.get_json()
-    base64Image=insertValues['image']
+    if request.method == 'POST' and 'image'  in request.files:
+      image = request.files['image']  
+      base64Image = str(base64.b64encode(image.read()))
+      base64Image=base64Image.split('\'')[1]
+    elif request.method == 'POST' and 'image'  in request.get_json():
+      insertValues = request.get_json()
+      base64Image=insertValues['image']
+    # return base64Image
     return jsonify({'result':str(mnistModule.getResult(base64Image)),'image':base64Image})
   
